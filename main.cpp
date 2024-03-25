64ddc498e36785e066e18f882c391b42eda1a8cd
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-03-24 23:11:27 -0300

    exercício 1 feito

--- main.cpp
+++ main.cpp
@@ -1,10 +1,71 @@
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
 
-void ex1(), ex2(), ex3(), ex4(), ex5(), ex6();
+void ex1(), ex2(), ex3(), ex4(), ex5(), ex6(), ex7(), ex8(), ex9(), ex10(), ex11();
 
-int main(){
-    
-
+int main()
+{
+    ex2();
+    // system("pause");
     return 0;
 }
+
+void ex1()
+{
+    float num1, num2;
+
+    printf("Digite o primeiro numero:");
+    scanf("%f", &num1);
+    printf("Digite o segundo numero:");
+    scanf("%f", &num2);
+    printf("Soma: %.2f\n Subtracao: %.2f\n Multiplicacao: %.2f\n Divisao: %.2f\n", num1 + num2, num1 - num2, num1 * num2, num1 / num2);
+}
+
+void ex2()
+{
+    
+}
+void ex3() {}
+void ex4() {}
+void ex5() {}
+void ex6() {}
+void ex7() {}
+void ex8() {}
+void ex9() {}
+void ex10() {}
+void ex11() {}
+
+/*1– Faça um programa que recebe dois valores e apresente na tela a soma, a subtração e
+multiplicação e a divisão desses dois valores.
+2 c2ex18) – Escreva um programa que solicite ao usuário a altura e o raio de um
+cilindro circular e imprima seu volume. Use a fórmula:
+Volume de um cilindro = 3,141592 * raio * raio * altura
+3 – Escreva um programa que receba uma medida em metros e retorne essa quantidade
+em centímetros.
+4 – Escreva um programa que receba uma medida em centímetros e retorne essa
+quantidade em metros.
+5 – Escreva um programa que receba 5 valores e retorne a média entre eles.
+6 – Escreva um programa que leia o preço de uma refeição e retorne o valor dos 10% da
+conta.
+7 (c2ex22) – Sabe-se que o latão é obtido por uma mistura de 7 partes de cobre e 3
+partes de zinco, ou seja, 70% do peso de latão é formado por cobre e 30% de zinco.
+Escreva um programa que receba o valor em kgs de quanto de latão deve ser produzido,
+e o programa deve indicar quantos kgs de cobre e zinco serão necessários.
+8 (c2ex23) – Um trabalhador recebe R$ 100,00 por dia trabalhado. Ao receber, são
+descontados 8% de impostos. Escreva um programa que receba o número de dias
+trabalhados e informe o quanto deve ser pago ao trabalhador.
+9 – Sabe-se que o preço de um piso é de R$ 7,00 o m2 e que cada peça possui 0,25 m2
+.
+Escreva um programa que receba uma área (em metros quadrados) e retorne quantas
+peças serão necessárias e o valor total.
+10 - Escrever um programa que lê a identificação de um vendedor (a identificação pode
+ser um número inteiro qualquer), o seu salário fixo, o total das vendas por ele efetuadas
+e o percentual que ganha sobre o total de vendas. O programa deve calcular o salário
+total do vendedor e apresentar na tela a identificação do vendedor e o seu salário total.
+11 - Escreva um programa que receba três números inteiros e positivos (A, B, C) e
+calcule a seguinte expressão:
+D = (R + S) / 2
+Sendo R = (A + B)2 e S = (B + C) 2
+.
+Dica: para o cálculo do quadrado, neste momento, use x * x*/
