9b157eda29067e6ace039c83b31a027e67d0f790
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-03-25 00:00:05 -0300

    exercicio 3 em produção

--- main.cpp
+++ main.cpp
@@ -42,42 +42,48 @@ void ex2()
     scanf("%f", &raio);
     printf("\n O volume do cilindro eh de: %.2fcm^3", altura * (pow(raio, 2) * M_PI));
 }
-void ex3() {}
-void ex4() {}
-void ex5() {}
-void ex6() {}
-void ex7() {}
-void ex8() {}
-void ex9() {}
-void ex10() {}
-void ex11() {}
 
-/*
-3 – Escreva um programa que receba uma medida em metros e retorne essa quantidade
-em centímetros.
-4 – Escreva um programa que receba uma medida em centímetros e retorne essa
-quantidade em metros.
-5 – Escreva um programa que receba 5 valores e retorne a média entre eles.
-6 – Escreva um programa que leia o preço de uma refeição e retorne o valor dos 10% da
-conta.
-7 (c2ex22) – Sabe-se que o latão é obtido por uma mistura de 7 partes de cobre e 3
+/*3 – Escreva um programa que receba uma medida em metros e retorne essa quantidade
+em centímetros.*/
+void ex3() {}
+
+/*4 – Escreva um programa que receba uma medida em centímetros e retorne essa
+quantidade em metros.*/
+void ex4() {}
+
+/*5 – Escreva um programa que receba 5 valores e retorne a média entre eles.*/
+void ex5() {}
+
+/*6 – Escreva um programa que leia o preço de uma refeição e retorne o valor dos 10% da
+conta.*/
+void ex6() {}
+
+/*7 (c2ex22) – Sabe-se que o latão é obtido por uma mistura de 7 partes de cobre e 3
 partes de zinco, ou seja, 70% do peso de latão é formado por cobre e 30% de zinco.
 Escreva um programa que receba o valor em kgs de quanto de latão deve ser produzido,
-e o programa deve indicar quantos kgs de cobre e zinco serão necessários.
-8 (c2ex23) – Um trabalhador recebe R$ 100,00 por dia trabalhado. Ao receber, são
+e o programa deve indicar quantos kgs de cobre e zinco serão necessários.*/
+void ex7() {}
+
+/*8 (c2ex23) – Um trabalhador recebe R$ 100,00 por dia trabalhado. Ao receber, são
 descontados 8% de impostos. Escreva um programa que receba o número de dias
-trabalhados e informe o quanto deve ser pago ao trabalhador.
-9 – Sabe-se que o preço de um piso é de R$ 7,00 o m2 e que cada peça possui 0,25 m2
-.
+trabalhados e informe o quanto deve ser pago ao trabalhador.*/
+void ex8() {}
+
+/*9 – Sabe-se que o preço de um piso é de R$ 7,00 o m2 e que cada peça possui 0,25 m2.
 Escreva um programa que receba uma área (em metros quadrados) e retorne quantas
-peças serão necessárias e o valor total.
-10 - Escrever um programa que lê a identificação de um vendedor (a identificação pode
+peças serão necessárias e o valor total.*/
+void ex9() {}
+
+/*10 - Escrever um programa que lê a identificação de um vendedor (a identificação pode
 ser um número inteiro qualquer), o seu salário fixo, o total das vendas por ele efetuadas
 e o percentual que ganha sobre o total de vendas. O programa deve calcular o salário
-total do vendedor e apresentar na tela a identificação do vendedor e o seu salário total.
-11 - Escreva um programa que receba três números inteiros e positivos (A, B, C) e
+total do vendedor e apresentar na tela a identificação do vendedor e o seu salário total.*/
+void ex10() {}
+
+/*11 - Escreva um programa que receba três números inteiros e positivos (A, B, C) e
 calcule a seguinte expressão:
 D = (R + S) / 2
 Sendo R = (A + B)2 e S = (B + C) 2
 .
 Dica: para o cálculo do quadrado, neste momento, use x * x*/
+void ex11() {}
